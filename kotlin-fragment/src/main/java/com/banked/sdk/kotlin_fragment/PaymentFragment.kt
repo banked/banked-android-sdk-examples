db24d987a3a755538e1044a6a945d67bec1023a1
Ross Bines <ross@rbinesconsulting.co.uk>
2021-04-27 16:12:15 +0100

    Feature/initial version (#1)
    * Initial version of example project for the new Android SDK
    
    * README update
    
    * Code clean up
    
    * Dependency clean up

---
+++ kotlin-fragment/src/main/java/com/banked/sdk/kotlin_fragment/PaymentFragment.kt
@@ -0,0 +1,63 @@
+package com.banked.sdk.kotlin_fragment
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import com.banked.checkout.Banked
+
+private const val API_KEY = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+private const val CONTINUE_URL = "XXXXXXXX"
+private const val PAYMENT_ID = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+
+class PaymentFragment : Fragment(), Banked.OnPaymentSessionListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        Banked.onPaymentSessionListener = this
+        Banked.apiKey = API_KEY
+        Banked.apiServer = Banked.Server.STAGING
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<Button>(R.id.make_payment).setOnClickListener { _ ->
+            Banked.startPayment(
+                fragment = this,
+                paymentId = PAYMENT_ID,
+                continueUrl = CONTINUE_URL
+            )
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Banked.onStart(fragment = this)
+    }
+
+    override fun onPaymentFailed() {
+        Log.d("Banked", "Payment failed")
+    }
+
+    override fun onPaymentSuccess(
+        paymentId: String,
+        amountFormatted: String,
+        providerName: String,
+        payeeName: String
+    ) {
+        Log.d("Banked", "Payment success")
+    }
+}
---
+++ navigation-components/src/main/java/com/banked/sdk/navigation_components/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.banked.sdk.navigation_components
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        supportFragmentManager.beginTransaction()
+            .add(R.id.fragment_container, PaymentFragment())
+            .commit()
+    }
+}
